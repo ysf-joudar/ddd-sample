cd18cd1dfd9392cdca8f36e86e5f19031b7d02f3
bnasslahsen <badr.nasslahsen@gmail.com>
2018-05-27 13:46:06 +0200

    project review

--- ddd-sample-domaine/src/main/java/fr/training/spring/shop/domain/customer/CustomerEntity.java
+++ ddd-sample-domaine/src/main/java/fr/training/spring/shop/domain/customer/CustomerEntity.java
@@ -1,14 +1,10 @@
 package fr.training.spring.shop.domain.customer;
 
-import java.util.UUID;
-
 import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
 
-public class CustomerEntity {
+import fr.training.spring.shop.domain.common.BaseEntity;
 
-	@NotNull
-	private String id = UUID.randomUUID().toString();
+public class CustomerEntity extends BaseEntity {
 
 	@Valid
 	private CustomerVO customerVO;
@@ -27,39 +23,6 @@ public class CustomerEntity {
 
 	public void setCustomerVO(CustomerVO customerVO) {
 		this.customerVO = customerVO;
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		CustomerEntity other = (CustomerEntity) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
 	}
 
 }
--- ddd-sample-domaine/src/main/java/fr/training/spring/shop/domain/item/ItemEntity.java
+++ ddd-sample-domaine/src/main/java/fr/training/spring/shop/domain/item/ItemEntity.java
@@ -1,17 +1,13 @@
 package fr.training.spring.shop.domain.item;
 
 import java.util.List;
-import java.util.UUID;
 
 import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
 
+import fr.training.spring.shop.domain.common.BaseEntity;
 import fr.training.spring.shop.domain.order.OrderEntity;
 
-public class ItemEntity {
-
-	@NotNull
-	private String id = UUID.randomUUID().toString();
+public class ItemEntity extends BaseEntity {
 
 	@Valid
 	private ItemVO itemVO;
@@ -40,38 +36,5 @@ public class ItemEntity {
 
 	public void setOrders(List<OrderEntity> orders) {
 		this.orders = orders;
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		ItemEntity other = (ItemEntity) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
 	}
 }
